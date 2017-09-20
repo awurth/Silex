9e938860584db8c4f0feb40bbc30e30444c1f601
Alexis Wurth <alexis.wurth57@gmail.com>
2017-09-20 15:26:14 +0200

    Updated awurth/silex-user to version 0.5

--- app/config/config.php
+++ app/config/config.php
@@ -58,6 +58,7 @@ $app['swiftmailer.options'] = [
 
 // https://github.com/awurth/silex-user
 $app['silex_user.options'] = [
+    'object_manager' => 'orm.em',
     'user_class' => User::class,
     'firewall_name' => 'main',
     'use_templates' => false,
