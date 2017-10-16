3167befce53fbc27985416b4bac09d947ae8f123
Alexis Wurth <alexis.wurth57@gmail.com>
2017-10-16 17:19:06 +0200

    Fixed tests configuration

--- tests/app.php
+++ tests/app.php
@@ -1,18 +1,16 @@
 <?php
 
-use Silex\Application;
-
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new Application();
+$app = new Silex\Application();
 
 $app['debug'] = true;
 $app['root_dir'] = dirname(__DIR__);
 $app['environment'] = 'test';
 
-require __DIR__ . '/../app/providers_dev.php';
+require __DIR__ . '/../app/providers.dev.php';
 
-require __DIR__ . '/../app/config/config_test.php';
+require __DIR__ . '/../app/config/config.test.php';
 
 require __DIR__ . '/../app/controllers.php';
 
